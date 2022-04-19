77a7f2989d227d1f4a113191161911905aa6b4b7
Anna <45663784+AnBlsvv@users.noreply.github.com>
2022-04-19 20:22:08 +0700

    cv_site

---
+++ js/change_lang.js
@@ -0,0 +1,20 @@
+
+
+$('[lang="ru"]').hide();
+
+$('#lang-switch').change(function () {
+  var lang = $(this).val();
+  switch (lang) {
+    case 'en': 
+      $('[lang]').hide();
+      $('[lang="en"]').show();
+      break;
+    case 'ru':
+      $('[lang]').hide();
+      $('[lang="ru"]').show();
+      break;
+    default:
+      $('[lang]').hide();
+      $('[lang="en"]').show();
+  }
+});
